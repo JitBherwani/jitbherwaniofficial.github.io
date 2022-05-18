95ca3a2e248fdf0aed9f775045b25bfa585aaecb
JitBherwani <jitbherwani92@gmail.com>
2022-05-18 18:09:57 +0530

    updated

--- portfolio/index.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('nav');
-
-burger.addEventListener('click', ()=>{
-    nav.classList.toggle('hide');
-    nav.classList.toggle('nav')
-})
